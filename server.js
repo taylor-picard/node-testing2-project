c4ec001d03ee3612c1218bfea9577eb83c11a4cd
Taylor Picard <taylorkpicard1998@gmail.com>
2023-10-22 16:19:11 -0500

    router wired & scripts added

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const server = express();
+const NamesRouter = require('./api/namesRouter');
 
 server.use(express.json())
+
+server.use('/api/names', NamesRouter);
 
 module.exports = server;
